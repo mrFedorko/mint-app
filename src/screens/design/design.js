083dea57441dd32d557fed7ff17d3bffc003850c
doblil <doblil@mail.ru>
2022-09-13 13:05:43 +0300

    init design page

--- src/screens/design/design.js
+++ src/screens/design/design.js
@@ -3,9 +3,19 @@ import React, { Component } from "react";
 export default class Design extends Component{
     render = () => {
         return(
-            <>
-                <h1 style={{"padding" : "160px"}}>This is design page</h1>
-            </>
+            <section className="signs">
+                <div className="container">
+                    <div className="signs__header">
+                        <div className="signs__buttons">
+                            <button onClick={() => this.stateActiveSetter('useful')} className="signs__button btn btn-light">Полезная информация</button>
+                            <button  onClick={() => this.stateActiveSetter('calc')}  className="signs__button btn btn-light">Онлайн калькулятор</button>
+                            <button onClick={() => this.stateActiveSetter('order')}  className="signs__button btn btn-light">Как заказать</button>
+                        </div>
+                        <h1 className="signs__title">Вывески</h1>
+                    </div>
+                   {/* {active === 'useful' ? <SignsUseful onActiveChange = {(a) => this.stateActiveSetter(a) }/> : active === 'calc' ? <SignCalculator/> : active === 'order' ? <SignsOrder/> : <SignsUseful/>} */}
+                </div>
+           </section>
         )
     }
 }
