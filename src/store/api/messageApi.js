f442a8e9f25ae2ca8ddef82e7b3a7b5cc56cf0e7
Mrfedorko <mr_fedorko@mail.ru>
2022-11-12 21:46:20 +0300

    starting chat dev

--- src/store/api/messageApi.js
+++ src/store/api/messageApi.js
@@ -1,16 +1,57 @@
-import { api } from "./api";
+import { api } from './api.js'
+import { io } from 'socket.io-client';
+import c from '../../config.json'
 
 
+const events = {
+    sendMessage: 'sendMessage',
+    getMessages: 'messages',
+}
 
-export const notesApi = api.injectEndpoints({
+
+export const socketApi = api.injectEndpoints({
     endpoints: builder => ({
+        sendMessage: builder.mutation({
+            queryFn: (msg) => {
+              const socket = io(c.baseApiUrl,{withCredentials: true});
+              return new Promise(resolve => {
+                socket.emit(events.sendMessage, msg, (message) => {
+                  resolve({ data: message });
+                });
+              })
+            },
+          }),
         getMessages: builder.query({
-            query: (id) => ({
-                url: `/messages/chanel`,
-            }),
+            query: () => ({data:[]}),
+            async onCacheEntryAdded(
+                _, {cacheDataLoaded, cacheEntryRemoved, updateCachedData}
+            ){
+                try {
+                    await cacheDataLoaded;
+                    const socket = io(c.baseApiUrl,{withCredentials: true});
+
+                    socket.on('connect', () => {
+                        socket.emit(events.getMessages);
+                    });
+
+                    socket.on(events.sendMessage, (msg) =>{
+                        updateCachedData((data) => {
+                            data = [...data, msg]
+                        } )
+                    } );
+
+                    await cacheEntryRemoved;
+ 
+                    socket.off('connect');
+                    socket.off(events.sendMessage);
+
+
+                } catch (error) {
+                    console.error(error)
+                }
+            }
         }),
     }),
 });
 
-export const { useGetMessagesQuery } = notesApi;
-const ws = new WebSocket()
+export const { useGetMessagesQuery, useSendMessageMutation } = socketApi;
